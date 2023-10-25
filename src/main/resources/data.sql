d52e2c83d14758b14ba2812fddc512340df48097
katai2099 <kataix2@gmail.com>
2023-10-26 00:34:13 +0200

    add spring profiles

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert ignore into order_status (name) values ('ORDER PLACED');
+insert ignore into order_status (name) values ('PROCESSING');
+insert ignore into order_status (name) values ('OUT FOR DELIVERY');
+insert ignore into order_status (name) values ('DELIVERED');
+
+insert ignore into sizes (name) values ('S');
+insert ignore into sizes (name) values ('M');
+insert ignore into sizes (name) values ('L');
+insert ignore into sizes (name) values ('XL');
