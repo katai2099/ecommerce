5b05ed6599d083be7873c3b82d2ecb2a946eeb4d
katai2099 <kataix2@gmail.com>
2023-10-30 00:28:56 +0100

    fix stripe api key typo

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ echo "Deploying to EC2 instance"
 set -euo pipefail
 docker rmi -f katai2099/ecommerce-backend || true
 docker rm -f ecommerce-backend || true
-docker run --name ecommerce-backend -d -p 5000:8081 -v ~/.aws/:/root/.aws:ro --restart=always -e MAIL_SERVER_USERNAME=$MAIL_SERVER_USER -e MAIL_SERVER_PASSWORD=$MAIL_SERVER_PASS -e JWT_SIGNING_KEY=$JWT_SECRET -e STRIPE_API_KEY=$STRIP_API_KEY -e RDS_USERNAME=$RDS_USER -e RDS_PASSWORD=$RDS_PASS katai2099/ecommerce-backend
+docker run --name ecommerce-backend -d -p 5000:8081 -v ~/.aws/:/root/.aws:ro --restart=always -e MAIL_SERVER_USERNAME=$MAIL_SERVER_USER -e MAIL_SERVER_PASSWORD=$MAIL_SERVER_PASS -e JWT_SIGNING_KEY=$JWT_SECRET -e STRIPE_API_KEY=$STRIPE_API_KEY -e RDS_USERNAME=$RDS_USER -e RDS_PASSWORD=$RDS_PASS katai2099/ecommerce-backend
 EOF
