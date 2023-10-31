ad41a9786c8d715be5752e7175b424849272ba67
katai2099 <kataix2@gmail.com>
2023-11-01 00:20:11 +0100

    allow OPTIONS method in SecurityConfiguration

--- src/main/java/com/web/ecommerce/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/web/ecommerce/configuration/security/SecurityConfiguration.java
@@ -78,7 +78,7 @@ public class SecurityConfiguration {
                 "http://127.0.0.1:3000",
                 "http://89.134.215.228:3000",
                 "https://ecommerce-frontend-henna-three.vercel.app"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type","Cookie"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
