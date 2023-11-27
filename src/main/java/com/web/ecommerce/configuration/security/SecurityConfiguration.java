cd18d9c9e74b0ff6c63f91dd79eead880b6323fa
katai2099 <kataix2@gmail.com>
2023-11-27 21:30:32 +0100

    update cors header

--- src/main/java/com/web/ecommerce/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/web/ecommerce/configuration/security/SecurityConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -81,7 +80,7 @@ public class SecurityConfiguration {
                 "https://ecommerce-frontend-henna-three.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedHeaders(Arrays.asList("X-CSRF-Token", "X-Requested-With", "Accept", "Accept-Version", "Content-Length", "Content-MD5", "Content-Type", "Date", "X-Api-Version"));
         configuration.setMaxAge(3600L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
